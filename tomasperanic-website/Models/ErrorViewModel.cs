7b335f650928507de9f160f968caac78c25c2129
Tomas Peranic <peranic.tomas@hotmail.com>
2023-09-01 11:22:12 -0400

    Add project files.

---
+++ tomasperanic-website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tomasperanic_website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
